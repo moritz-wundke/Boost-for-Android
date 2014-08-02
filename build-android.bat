7464117d9838478665ebb78dd327c0f3a93fc11d
Antony Polukhin <antoshkka@gmail.com>
2014-08-02 22:53:36 +0400

    Removed doubtful copyright notice

--- build-android.bat
+++ build-android.bat
@@ -1,10 +1,5 @@
 @Echo Off
 
-REM          Copyright Antony Polukhin 2013.
-REM Distributed under the Boost Software License, Version 1.0.
-REM    (See accompanying file LICENSE_1_0.txt or copy at
-REM          http://www.boost.org/LICENSE_1_0.txt)
-
 sh -c "echo MSYS found. Running ./build-android.sh"
 If %ERRORLEVEL% EQU 0 GOTO MSYSOK
 echo This script requires MSYS installed and path to its bin folder added to PATH variable
