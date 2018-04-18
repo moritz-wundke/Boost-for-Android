633eb360edd3a0572419b84def6b7aadd65bd8f8
Ralph-Gordon Paul <gordon@rgpaul.com>
2018-04-18 11:29:15 +0200

    Support for Boost 1.67.0

--- build-android.sh
+++ build-android.sh
@@ -28,12 +28,16 @@
 # -----------------------
 
 BOOST_VER1=1
-BOOST_VER2=66
+BOOST_VER2=67
 BOOST_VER3=0
-register_option "--boost=<version>" boost_version "Boost version to be used, one of {1.66.0, 1.65.1, 1.55.0, 1.54.0, 1.53.0, 1.49.0, 1.48.0, 1.45.0}, default is 1.66.0."
+register_option "--boost=<version>" boost_version "Boost version to be used, one of {1.67.0, 1.66.0, 1.65.1, 1.55.0, 1.54.0, 1.53.0, 1.49.0, 1.48.0, 1.45.0}, default is 1.67.0."
 boost_version()
 {
   if [ "$1" = "1.66.0" ]; then
+    BOOST_VER1=1
+    BOOST_VER2=67
+    BOOST_VER3=0    
+  elif [ "$1" = "1.66.0" ]; then
     BOOST_VER1=1
     BOOST_VER2=66
     BOOST_VER3=0
