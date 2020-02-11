0f4b3b1cc7c9146547840ae47757e29e40ad2a77
Yuriy Solodkyy <yuriy.solodkyy@gmail.com>
2020-02-10 17:31:30 -0800

    Fixes an extra quote VSCode inserted and I didn't notice

--- build-android.sh
+++ build-android.sh
@@ -362,7 +362,7 @@ if [ -z "${ARCHLIST}" ]; then
 
     case "$NDK_RN" in
       # NDK 17+: Support for ARMv5 (armeabi), MIPS, and MIPS64 has been removed.
-      "17.1"|"17.2"|"18.0"|"18.1"|"19.0"|"19.1"|"19.2"|"20.0"|"20.1""|"21.0")
+      "17.1"|"17.2"|"18.0"|"18.1"|"19.0"|"19.1"|"19.2"|"20.0"|"20.1"|"21.0")
         ARCHLIST="arm64-v8a armeabi-v7a x86 x86_64"
         ;;
       *)
