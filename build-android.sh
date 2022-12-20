b49787f42f731d2ebd6de3c981108aa60440e851
SuperGNUS <ken@gnus.ai>
2022-12-20 11:59:35 -0800

    Branch to debug ci system patch command

--- build-android.sh
+++ build-android.sh
@@ -546,6 +546,7 @@ then
       PATCHNAME=`basename $PATCH`
       log "Applying $PATCHNAME into $SRC_DIR/$PATCHDIR"
       cd $SRC_DIR
+      patch --version
       echo "Running: `which patch` -p1 < $dir/$PATCH in `pwd`"
       patch -p1 < $dir/$PATCH
       if [ $? != 0 ] ; then
